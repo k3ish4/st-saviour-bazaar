ca43fbe8e916ef2abcbd6ee63075cb3dcea11a11
Keisha <k3ish4.v@gmail.com>
2026-02-13 09:09:51 -0500

    lab 5

--- Hello.java
+++ Hello.java
@@ -41,6 +41,12 @@ public static void main(String[] args) {
         System.out.println("");
     }
     
+    // String[][] diagonal = new String[11][11];
+    // for(int i = 0; i < 11; i++){
+    //     for(int j = 0; j < 11; j++){
+            
+    //     }
+    // }
 }
 
     // public static void weave(String[][] rug) {
